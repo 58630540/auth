3b80f5f87074683fbe2186daa1cb7cc82358f01b
liujun <58630540@qq.com>
2022-05-18 17:33:54 +0800

    增加C端授权中间件

--- src/Config/kabel_auth_cache.php
+++ src/Config/kabel_auth_cache.php
@@ -22,9 +22,9 @@ return [
     ],
     'cUserAuth' => [ //缓存驱动配置
         'url' => env('REDIS_URL'),
-        'host' => env('REDIS_HOST', '127.0.0.1'),
-        'password' => env('REDIS_PASSWORD', null),
-        'port' => env('REDIS_PORT', '6379'),
+        'host' => env('LOGIN_REDIS_HOST', '127.0.0.1'),
+        'password' => env('LOGIN_REDIS_PASSWORD', null),
+        'port' => env('LOGIN_REDIS_PORT', '6379'),
         'database' => env('REDIS_CACHE_DB', '2'),
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'predis'),
