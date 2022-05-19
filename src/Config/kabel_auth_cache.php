1a02731f12362ff17efd13352bdef2aa28e8ac5e
liujun <58630540@qq.com>
2022-05-19 23:53:11 +0800

    Update kabel_auth_cache.php

--- src/Config/kabel_auth_cache.php
+++ src/Config/kabel_auth_cache.php
@@ -14,7 +14,7 @@ return [
         'host' => env('REDIS_HOST', '127.0.0.1'),
         'password' => env('REDIS_PASSWORD', null),
         'port' => env('REDIS_PORT', '6379'),
-        'database' => env('REDIS_CACHE_DB', '1'),
+        'database' =>1,
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'predis'),
             'prefix' => '',
@@ -25,7 +25,7 @@ return [
         'host' => env('LOGIN_REDIS_HOST', '127.0.0.1'),
         'password' => env('LOGIN_REDIS_PASSWORD', null),
         'port' => env('LOGIN_REDIS_PORT', '6379'),
-        'database' => env('REDIS_CACHE_DB', '2'),
+        'database' => 2,
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'predis'),
             'prefix' => '',
@@ -36,7 +36,7 @@ return [
         'host' => env('REDIS_HOST', '127.0.0.1'),
         'password' => env('REDIS_PASSWORD', null),
         'port' => env('REDIS_PORT', '6379'),
-        'database' => env('REDIS_CACHE_DB', '3'),
+        'database' => 3,
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'predis'),
             'prefix' => '',
